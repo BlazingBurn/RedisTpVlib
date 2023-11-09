9355b52232ae53f88e6d1e97cbc8a7d08f3c759c
fuckredis <fuckredis@fuckredis-3.0>
2023-11-09 21:25:37 +0000

    update code furedis

--- flask_app/flaskvlib/routes.py
+++
@@ -1,5 +0,0 @@
-from flaskvlib import redis_client, app
-
-@app.route('/')
-def index():
-    return redis_client.hgetall('vlibid:8035')
--- flask_app/run.py
+++
@@ -1,5 +0,0 @@
-from flaskvlib import app
-
-if __name__ == '__main__':
-    
-    app.run()
